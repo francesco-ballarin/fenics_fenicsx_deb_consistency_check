e4d6f8af372671bd05b749b7c5ffc8eb2e28beb8
Francesco Ballarin <francesco.ballarin@unicatt.it>
2023-12-09 08:10:40 +0100

    Add test coverage for missing packages on import

---
+++ tests/data/pusimp_package_four/pusimp_package_four/__init__.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2023 by the pusimp authors
+#
+# This file is part of pusimp.
+#
+# SPDX-License-Identifier: MIT
+"""Mock package for pusimp tests.
+
+This package imports pusimp_dependency_one and pusimp_dependency_missing.
+pusimp_dependency_missing is a mandatory dependency and will always be missing, and therefore
+this package will raise an ImportError on import.
+"""
+
+import pusimp_golden_source
+
+import pusimp
+
+pusimp.prevent_user_site_imports(
+    "pusimp_package_four", pusimp_golden_source.system_package_manager, pusimp_golden_source.contact_url,
+    pusimp_golden_source.system_path,
+    ["pusimp_dependency_one", "pusimp_dependency_missing"],
+    ["pusimp-dependency-one", "pusimp-dependency-missing"],
+    [False, False],
+    ["", ""]
+)
+
+import pusimp_dependency_missing  # noqa: E402, F401
+import pusimp_dependency_one  # noqa: E402, F401
--- tests/unit/test_data.py
+++ tests/unit/test_data.py
@@ -3,18 +3,41 @@
 # This file is part of pusimp.
 #
 # SPDX-License-Identifier: MIT
-"""Test that all mock packages in tests/data import correctly after installation."""
+"""Test imports of mock packages in tests/data."""
+
+import os
+
+import pytest
+
+import pusimp_golden_source  # isort: skip
+
 
 def test_data_one() -> None:
-    """Test that the first mock package in tests/data import correctly after installation."""
+    """Test that the first mock package in tests/data import correctly."""
     import pusimp_package_one  # noqa: F401
 
 
 def test_data_two() -> None:
-    """Test that the second mock package in tests/data import correctly after installation."""
+    """Test that the second mock package in tests/data import correctly."""
     import pusimp_package_two  # noqa: F401
 
 
 def test_data_three() -> None:
-    """Test that the third mock package in tests/data import correctly after installation."""
+    """Test that the third mock package in tests/data import correctly."""
     import pusimp_package_three  # noqa: F401
+
+
+def test_data_four() -> None:
+    """Test that the fourth mock package in tests/data fails to import due to a missing mandatory dependency."""
+    with pytest.raises(ImportError) as excinfo:
+        import pusimp_package_four  # noqa: F401
+    import_error_text = str(excinfo.value)
+    assert import_error_text == (
+        "pusimp_dependency_missing is missing. Its expected path was "
+        f"{os.path.join(pusimp_golden_source.system_path, 'pusimp_dependency_missing', '__init__.py')}."
+    )
+
+
+def test_data_five() -> None:
+    """Test that the fifth mock package in tests/data import correctly, because the missing dependency is optional."""
+    import pusimp_package_five  # noqa: F401
